1ead6a395facedbdf24ecfadbbcbcc836bab24e2
efrenbg1 <efren@boyarizo.es>
2024-01-01 20:50:01 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# LGFanControl
-LG Gram Fan Control
+## LG Gram Automatic Fan Control
+
+#### Why?
+> New LG Grams with 13th gen CPUs are hot. Nothing we can't change there.
+> 
+> When connected to the AC, even normal browsing will keep the cooling fan ON most of the time which can be quite annoying. LG provides four modes for cooling, but changing between them all the time is just a pain in the ass...
+
+#### What does it do?
+> To solve it I've created a Python script which reads the temperature every 15 seconds and switches between Normal and Silent mode automatically.
+
+#### How it works?
+> To read the CPU package temperature I use `LibreHardwareMonitorLib.dll`
+>
+> To change between cooling modes we need two DLLs:
+> 1. `WMIConn.dll`: provides the communication to WMI
+> 1. `WindowsControl.dll`: contains the interesting functions to change the cooling mode (and much more things!)
+>
+> Both DLLs are located under the directory `C:\Program Files (x86)\LG Software\LG Smart Assistant`
+>
+> To visualize the available functions you can use (is free): https://www.jetbrains.com/es-es/decompiler/
+>
+> The program must run as ADMIN, to have it run on startup you can use Task Scheduler in Windows. An example of the task can be found in the file `WindowsTask.xml`. You can import it directly into the Task Scheduler
---
+++ service.py
@@ -0,0 +1,62 @@
+import sys
+import ctypes
+import time
+import clr
+import os
+
+# Load DLL for reading temperature
+LibreHardwareDLL = os.path.dirname(os.path.abspath(sys.argv[0]))
+LibreHardwareDLL = os.path.join(LibreHardwareDLL, "LibreHardwareMonitorLib.dll")
+clr.AddReference(LibreHardwareDLL)
+
+# Load DLL for changing fan cooling mode (must have installed the new version of LG Smart Assistant)
+clr.AddReference(
+    "C:\\Program Files (x86)\\LG Software\\LG Smart Assistant\\WMIConn.dll")
+clr.AddReference(
+    "C:\\Program Files (x86)\\LG Software\\LG Smart Assistant\\WindowsControl.dll")
+
+
+# We need admin privileges to read temperature from CPU
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if not is_admin():
+    ctypes.windll.shell32.ShellExecuteW(
+        None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    exit()
+
+
+# Initialize the classes from the DLLs
+if True:
+    from LibreHardwareMonitor.Hardware import Computer
+    from WindowsControl import LGDeviceController
+
+lg = LGDeviceController()
+c = Computer()
+c.IsCpuEnabled = True
+
+# Set the last cooling mode state to the currently set one
+lastState = lg.Get_SystemTempMode()
+
+# We loop every 15 seconds and change the cooling mode accordingly
+while True:
+
+    # Get the sensor for reading the temperature (should be the 30th on the list)
+    c.Open()
+    d = c.Hardware[0].Sensors[30]
+    c.Close()
+
+    # If CPU temperature is greater than 75ºC we switch to Normal mode
+    if d.Value > 75 and lastState != 1:
+        lg.Set_SystemTempMode(1)
+        lastState = 1
+    
+    # If CPU temperature drops below 60ºC we switch to Silent mode 
+    if d.Value < 60 and lastState != 3:
+        lg.Set_SystemTempMode(3)
+        lastState = 3
+
+    time.sleep(15)
